9b35a211352b9ad7b6b34bb910a28d48dfe4d4f3
NikolaIv <poormuggle6@gmail.com>
2024-01-26 12:35:09 +0200

    Implemented LecturesApiController with base endpoints.

--- StudyJunction/StudyJunction.Web/Controllers/API/LecturesApiController.cs
+++ StudyJunction/StudyJunction.Web/Controllers/API/LecturesApiController.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using StudyJunction.Core.RequestDTOs;
+using StudyJunction.Core.Services;
+using StudyJunction.Core.Services.Contracts;
+using StudyJunction.Infrastructure.Exceptions;
+using StudyJunction.Infrastructure.Repositories.Contracts;
 
 namespace StudyJunction.Web.Controllers.API
 {
@@ -7,5 +12,99 @@ namespace StudyJunction.Web.Controllers.API
 	[ApiController]
 	public class LecturesApiController : ControllerBase
 	{
+		private ILectureService lectureService;
+        public LecturesApiController(ILectureService _lectureService)
+        {
+            lectureService = _lectureService;
+        }
+
+        [HttpGet("{id}")]
+		public IActionResult Get(string id)
+		{
+			try
+			{
+				var lecture = lectureService.Get(new Guid(id));
+				return Ok(lecture);
+			}
+			catch (EntityNotFoundException ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+
+		[HttpGet("{title}")]
+		public IActionResult GetByTitle(string title)
+		{
+			try
+			{
+				var lecture = lectureService.Get(title);
+				return Ok(lecture);
+			}
+			catch (EntityNotFoundException e)
+			{
+				return BadRequest(e.Message);
+			}
+		}
+
+		[HttpGet("")]
+		public IActionResult GetCourses()
+		{
+			var courses = lectureService.GetAll();
+			return Ok(courses);
+		}
+
+		[HttpPost("")]
+		public IActionResult CreateLecture([FromBody] AddLectureRequestDto newLecture)
+		{
+			try
+			{
+				var lecture = lectureService.Create(newLecture);
+				return Ok(lecture);
+			}
+			catch (UnauthorizedUserException e)
+			{
+				return Unauthorized(e.Message);
+			}
+			catch (NameDuplicationException ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult UpdateLecture(string id, [FromBody] LectureRequestDto newData)
+		{
+			try
+			{
+				var updated = lectureService.Update(new Guid(id), newData);
+				return Ok(updated);
+			}
+			catch (UnauthorizedUserException e)
+			{
+				return Unauthorized(e.Message);
+			}
+			catch (EntityNotFoundException ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult DeleteLecture(string id)
+		{
+			try
+			{
+				var deleted = lectureService.Delete(new Guid(id));
+				return Ok(deleted);
+			}
+			catch (UnauthorizedUserException e)
+			{
+				return Unauthorized(e.Message);
+			}
+			catch (EntityNotFoundException e)
+			{
+				return BadRequest(e.Message);
+			}
+		}
 	}
 }
