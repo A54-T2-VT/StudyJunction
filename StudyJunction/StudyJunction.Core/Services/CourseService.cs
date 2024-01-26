4924f152d56521b24beb2d65e81d0159dc2d225f
Nikolay Dobrev <nickndobrev@gmail.com>
2024-01-26 11:47:27 +0200

    Added interface funs in services, not  implemented.

--- StudyJunction/StudyJunction.Core/Services/CourseService.cs
+++ StudyJunction/StudyJunction.Core/Services/CourseService.cs
@@ -1,15 +1,39 @@
-﻿using StudyJunction.Core.Services.Contracts;
-using StudyJunction.Infrastructure.Repositories.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using StudyJunction.Core.RequestDTOs;
+using StudyJunction.Core.ResponseDTOs;
+using StudyJunction.Core.Services.Contracts;
 
 namespace StudyJunction.Core.Services
 {
-	public class CourseService : ICourseService
-	{
+    public class CourseService : ICourseService
+    {
+        public CourseResponseDTO Create(AddCourseRequestDto newCourse)
+        {
+            throw new NotImplementedException();
+        }
 
-	}
+        public CourseResponseDTO Delete(Guid toDelete)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<CourseResponseDTO> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CourseResponseDTO GetCourse(Guid courseId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CourseResponseDTO GetCourse(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CourseResponseDTO Update(Guid toUpdate, CourseRequestDto newData)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
--- StudyJunction/StudyJunction.Core/Services/NoteService.cs
+++ StudyJunction/StudyJunction.Core/Services/NoteService.cs
@@ -1,13 +1,34 @@
-﻿using StudyJunction.Core.Services.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using StudyJunction.Core.RequestDTOs;
+using StudyJunction.Core.ResponseDTOs;
+using StudyJunction.Core.Services.Contracts;
 
 namespace StudyJunction.Core.Services
 {
-	public class NoteService : INoteService
-	{
-	}
+    public class NoteService : INoteService
+    {
+        public NoteResponseDTO Create(AddNoteRequestDto newNote)
+        {
+            throw new NotImplementedException();
+        }
+
+        public NoteResponseDTO Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<NoteResponseDTO> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public NoteResponseDTO GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public NoteResponseDTO Update(Guid id, NoteRequestDto updatedNote)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
