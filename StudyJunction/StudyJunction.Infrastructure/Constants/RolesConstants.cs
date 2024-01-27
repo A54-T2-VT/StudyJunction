6f06e2441830e08b9ff90a8a9ed186f3b519a568
Nikolay Dobrev <nickndobrev@gmail.com>
2024-01-27 19:35:48 +0200

    Made UserRoles int RolesConstants with strings.

---
+++ StudyJunction/StudyJunction.Infrastructure/Constants/RolesConstants.cs
@@ -0,0 +1,11 @@
+﻿
+namespace StudyJunction.Infrastructure.Constants
+{
+    public static class RolesConstants
+    {
+        public const string Student = "Student";
+        public const string Teacher = "Teacher";
+        public const string Admin = "Admin";
+        public const string God = "God";
+    }
+}
