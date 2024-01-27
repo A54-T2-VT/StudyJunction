a1ebbfc0d18ddc5475b1bd6ef63a2cf30624c5b7
Nikolay Dobrev <nickndobrev@gmail.com>
2024-01-27 19:34:14 +0200

    Changed Id type from Guid to string.

--- StudyJunction/StudyJunction.Core/ResponseDTOs/UserResponseDTO.cs
+++ StudyJunction/StudyJunction.Core/ResponseDTOs/UserResponseDTO.cs
@@ -4,7 +4,7 @@ namespace StudyJunction.Core.ResponseDTOs
 {
 	public class UserResponseDTO
 	{
-        public Guid Id { get; set; }
+        public string Id { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
         public string FirstName { get; set; }
