4fb8342850c5711363c5848a0add679d25f79016
Nikolay Dobrev <nickndobrev@gmail.com>
2024-01-28 19:38:05 +0200

    Added exception message for invalid email.

--- StudyJunction/StudyJunction.Infrastructure/Constants/ExceptionMessages.cs
+++ StudyJunction/StudyJunction.Infrastructure/Constants/ExceptionMessages.cs
@@ -10,8 +10,9 @@ namespace StudyJunction.Infrastructure.Constants
 	{
 		public const string USER_WITH_ID_NOT_FOUND_MESSAGE = "User with id [{0}] not found.";
 		public const string USER_WITH_USERNAME_NOT_FOUND_MESSAGE = "User with username [{0}] not found.";
+        public const string USER_WITH_EMAIL_NOT_FOUND_MESSAGE = "User with email [{0}] not found.";
 
-		public const string COURSE_WITH_TITLE_NOT_FOUND_MESSAGE = "Course with title [{0}] not found.";
+        public const string COURSE_WITH_TITLE_NOT_FOUND_MESSAGE = "Course with title [{0}] not found.";
 		public const string COURSE_WITH_ID_NOT_FOUND_MESSAGE = "Course with id [{0}] not found.";
 
 		public const string LECTURE_WITH_ID_NOT_FOUND_MESSAGE = "Lecture with id [{0}] not found.";
