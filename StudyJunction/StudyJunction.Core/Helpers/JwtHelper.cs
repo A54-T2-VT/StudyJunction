29e287041bbfc31e8eb0e87c91cea96b6209f296
Nikolay Dobrev <nickndobrev@gmail.com>
2024-02-02 13:12:03 +0200

    Added func to get NameIdentifier from jwt in JwtHelper.

--- StudyJunction/StudyJunction.Core/Helpers/JwtHelper.cs
+++ StudyJunction/StudyJunction.Core/Helpers/JwtHelper.cs
@@ -34,5 +34,34 @@ namespace StudyJunction.Core.Helpers
             return username; // Return null if the name claim is not found
         }
 
+        public static string GetNameIdentifierClaimFromJwt(string jwt)
+        {
+            {
+                jwt = jwt.Replace("Bearer ", "");
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var jsonToken = tokenHandler.ReadToken(jwt) as JwtSecurityToken;
+
+                var id = string.Empty;
+
+                if (jsonToken != null)
+                {
+                    var nameClaim = jsonToken.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+
+                    if (nameClaim != null)
+                    {
+                        id = nameClaim.Value;
+                    }
+                }
+
+                if (id == string.Empty)
+                {
+                    throw new NotImplementedException();
+                }
+
+                return id; // Return null if the name claim is not found
+            }
+        }
+
     }
 }
