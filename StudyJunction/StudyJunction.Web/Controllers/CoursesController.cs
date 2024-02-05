6651b69871e3aae3ec5a59e684a2f7dd7fbbb3c8
NikolaIv <poormuggle6@gmail.com>
2024-02-05 23:39:28 +0200

    Added MVC CoursesController and Index view.

---
+++ StudyJunction/StudyJunction.Web/Controllers/CoursesController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using StudyJunction.Core.Services.Contracts;
+
+namespace StudyJunction.Web.Controllers
+{
+	public class CoursesController : Controller
+	{
+		private readonly ICourseService courseService;
+		private readonly IUserService userService;
+		private readonly IMapper mapper;
+        public CoursesController(ICourseService _courseService, IUserService _userService, IMapper _mapper)
+        {
+            courseService = _courseService;
+			userService = _userService;
+			mapper = _mapper;
+        }
+        public IActionResult Index()
+		{
+			var courses = courseService.GetAll();
+			return View(courses);
+		}
+	}
+}
