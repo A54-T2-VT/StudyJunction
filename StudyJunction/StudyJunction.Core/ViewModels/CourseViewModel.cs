82a9cbea51aff85e1d4f70a43f29e898c2cf04ab
NikolaIv <poormuggle6@gmail.com>
2024-02-08 14:36:54 +0200

    Added CourseViewModel class.

---
+++ StudyJunction/StudyJunction.Core/ViewModels/CourseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CloudinaryDotNet;
+using StudyJunction.Core.ExternalApis;
+using StudyJunction.Core.ResponseDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyJunction.Core.ViewModels
+{
+    public class CourseViewModel
+    {
+        public CloudinaryService Service { get; set; }
+        public ICollection<CourseResponseDTO> Courses { get; set; }
+    }
+}
