455cb4a7298e1687160df4fea31222800e4256b4
Nikolay Dobrev <nickndobrev@gmail.com>
2024-02-10 20:43:13 +0200

    Added controllers for Categories, Lectures and Users in mvc part.

---
+++ StudyJunction/StudyJunction.Web/Controllers/LecturesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudyJunction.Web.Controllers
+{
+    public class LecturesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ StudyJunction/StudyJunction.Web/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudyJunction.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
