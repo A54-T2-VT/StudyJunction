4d5502adbd4a2b536f8beac8d3ca21ed9e8f78c3
Nikolay Dobrev <nickndobrev@gmail.com>
2024-02-13 19:28:50 +0200

    Added user registretion in UsersController, need no check which exceptions needs to be catched and how to be handled.

--- StudyJunction/StudyJunction.Web/Controllers/UsersController.cs
+++ StudyJunction/StudyJunction.Web/Controllers/UsersController.cs
@@ -1,12 +1,66 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StudyJunction.Core.RequestDTOs.User;
+using StudyJunction.Core.Services.Contracts;
+using StudyJunction.Core.ViewModels.User;
+using StudyJunction.Infrastructure.Data.Models;
+using StudyJunction.Infrastructure.Exceptions;
 
 namespace StudyJunction.Web.Controllers
 {
     public class UsersController : Controller
     {
-        public IActionResult Index()
+        private readonly IUserService userService;
+        private readonly IMapper mapper;
+
+        public UsersController(IUserService userService,
+            IMapper mapper)
+        {
+            this.userService = userService;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Login(string delete)
+        {
+            throw new NotFiniteNumberException();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            var model = new RegisterViewModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            try
+            {
+                var registerDto = mapper.Map<RegisterUserRequestDto>(model);
+
+                _ = await userService.Register(registerDto);
+
+                return RedirectToAction("Login","Users");
+            }
+            catch(NameDuplicationException ex) 
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
