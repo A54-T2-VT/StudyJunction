7ee9ce16e5f1ba04a915f7e4cd99dd520c7d044d
Nikolay Dobrev <nickndobrev@gmail.com>
2024-02-13 19:56:01 +0200

    Added test project and create basic test structure.

---
+++ StudyJunction/StudyJunction.Test/NoteService/Create_Should.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyJunction.Test.NoteService
+{
+    internal class Create_Should
+    {
+    }
+}
---
+++ StudyJunction/StudyJunction.Test/UserService/Register_Should.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyJunction.Test.UserService
+{
+    internal class Register_Should
+    {
+    }
+}
