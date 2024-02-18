501c261aa1b74d6858cdd58621f803a5f8c28799
Nikolay Dobrev <nickndobrev@gmail.com>
2024-02-18 20:13:48 +0200

    Fixed bug preventing proper course creation in mvc part.

--- StudyJunction/StudyJunction.Core/ViewModels/Courses/CreateCourseViewModel.cs
+++ StudyJunction/StudyJunction.Core/ViewModels/Courses/CreateCourseViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper.Configuration.Annotations;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -12,8 +13,8 @@ namespace StudyJunction.Core.ViewModels.Courses
     public class CreateCourseViewModel
     {
         [Required]
-        [MinLength(20, ErrorMessage = "The {0} must be at least {1} characters long.")]
-        [MaxLength(100, ErrorMessage = "The {0} must be no more than {1} characters long.")]
+        [MinLength(5, ErrorMessage = "The {0} must be at least {1} characters long.")]
+        [MaxLength(50, ErrorMessage = "The {0} must be no more than {1} characters long.")]
         public string Title { get; set; }
         [Required]
         [MinLength(100, ErrorMessage = "The {0} must be at least {1} characters long.")]
@@ -25,7 +26,7 @@ namespace StudyJunction.Core.ViewModels.Courses
         [Required]
         public string CategoryName { get; set; }
         public DateTime StartDate { get; set; }
-
+        [ValidateNever]
         public Dictionary<string, List<string>> ParentChildCategories { get; set; }
     }
 }
