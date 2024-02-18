d77d66aebf76f1b67d548a2ca01b135e65fdf6b7
NikolaIv <poormuggle6@gmail.com>
2024-02-18 18:01:11 +0200

    Added MyCoursesViewModel to use in MyCourses view.

---
+++ StudyJunction/StudyJunction.Core/ViewModels/Courses/MyCoursesViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using StudyJunction.Core.ExternalApis;
+using StudyJunction.Core.ResponseDTOs;
+using StudyJunction.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyJunction.Core.ViewModels.Courses
+{
+    public class MyCoursesViewModel
+    {
+        public UserResponseDTO CurrentUser{ get; set; }
+        public CloudinaryService Service { get; set; }
+        public UserManager<UserDb> userManager { get; set; }
+    }
+}
