aa424d53aff3bb21ea5663af3eb5b9e58de6adf3
NikolaIv <poormuggle6@gmail.com>
2024-02-19 20:24:35 +0200

    Added EnrolledCourseViewModel.

---
+++ StudyJunction/StudyJunction.Core/ViewModels/Courses/EnrolledCourseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using StudyJunction.Core.ExternalApis;
+using StudyJunction.Core.ResponseDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyJunction.Core.ViewModels.Courses
+{
+    public class EnrolledCourseViewModel
+    {
+        public CourseResponseDTO Course { get; set; }
+        public CloudinaryService Service { get; set; }
+
+        public string Username { get; set; }
+    }
+}
