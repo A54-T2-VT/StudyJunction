bc65247f39ab7f2d6c8d8175c0ec0da25ed02883
Nikolay Dobrev <nickndobrev@gmail.com>
2024-02-20 14:01:26 +0200

    Fixed bug when after creating a new course was conflict in redirection. Added Lectures controller to Students controllers.

---
+++ StudyJunction/StudyJunction.Web/Areas/Student/Controllers/LecturesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudyJunction.Web.Areas.Student.Controllers
+{
+    public class LecturesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
